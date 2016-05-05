0cc157187da1e01b67a9a1521ecf4084ca2401af
Nithin David <webofnithin@gmail.com>
2016-05-06 01:05:20 +0530

    ember actions

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	isImageShowing: false,
+	actions: {
+	    imageShow() {
+	      this.toggleProperty('isImageShowing');
+	    }
+	}
 });
